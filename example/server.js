1117d17094f49f3de1a473cc7935aad5fa2079e8
HansHammel <oh.schitt@freenet.de>
2017-01-08 21:19:59 +0100

    fixt tests

--- example/server.js
+++ example/server.js
@@ -1,6 +1,35 @@
 (function() {
+
     var Locker = require("../index"),
         locker = new Locker();
 
+    process.on('SIGTERM', function() {
+        console.log('SIGTERM - Server closing...');
+        locker.close(function () {
+            process.exit(0);
+        });
+    });
+    process.on('SIGHUP', function() {
+        console.log('SIGHUP - Server closing...');
+        locker.close(function () {
+            process.exit(0);
+        });
+    });
+
+    process.on('SIGINT', function() {
+        console.log('SIGINT - Server closing...');
+        locker.close(function () {
+            process.exit(0);
+        });
+    });
+    process.on('exit', function() {
+        console.log('EXIT - Server closing...');
+        locker.close(function () {
+            process.exit(0);
+        });
+    });
+
+    console.log('Server started, listening on port 4545');
     locker.listen(4545);
+
 })();
